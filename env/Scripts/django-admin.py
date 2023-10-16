bbd3763f551609017811774fae8b704840ecead7
Gekif <dzulfikar.maulana@gmail.com>
2023-10-16 09:52:15 +0700

    View dan URL

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\pindahan\shopee 19 september 2023\1. paket belajar pemrograman\5 - belajar django dasar\file_latihan\mywebsite\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
