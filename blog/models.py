01ae9ef5645fc9ec4f15db8450149feb118d9f59
Gekif <dzulfikar.maulana@gmail.com>
2023-10-17 07:32:46 +0700

    Working With Form

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     content = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from blog import models
-from django.http import HttpResponse,  Http404
+from blog import models, forms
+from django.http import HttpResponse, Http404
 
 
 def index(request):
@@ -12,9 +12,14 @@ def index(request):
 
 def detail(request, id):
     try:
-        model = models.Post.objects.get(id = id)
+        model = models.Post.objects.get(id=id)
     except Exception:
         raise Http404()
+
+    form = forms.CommentForm(request.POST if request.method == 'POST' else {'post': model})
+
+    if form.is_valid():
+        form.save()
 
     return render(request, 'blog/detail.html', {
         'title': model.title,
