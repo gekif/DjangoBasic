0f28c8b8ef881e7e33fc2b6555105865681b39c1
Gekif <dzulfikar.maulana@gmail.com>
2023-10-17 07:50:59 +0700

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from blog.models import Post, Comment
+
+
 # Register your models here.
+class PostAmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'short_content']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post', 'content']
+
+
+admin.site.register(Post, PostAmin)
+admin.site.register(Comment, PostAmin)
